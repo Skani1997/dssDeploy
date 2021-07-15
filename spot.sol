37f5d305f0db9b2c7fece7e63a43a1c172ea0351
Skani1997 <63139286+Skani1997@users.noreply.github.com>
2021-07-15 17:21:04 +0900

    Create spot.sol

---
+++ spot.sol
@@ -0,0 +1,121 @@
+/**
+ *Submitted for verification at Etherscan.io on 2019-11-14
+*/
+
+// hevm: flattened sources of /nix/store/8xb41r4qd0cjb63wcrxf1qmfg88p0961-dss-6fd7de0/src/spot.sol
+pragma solidity >=0.5.12;
+
+contract LibNoteSpot {
+    event LogNote(
+        bytes4   indexed  sig,
+        address  indexed  usr,
+        bytes32  indexed  arg1,
+        bytes32  indexed  arg2,
+        bytes             data
+    ) anonymous;
+
+    modifier note {
+        _;
+        assembly {
+            // log an 'anonymous' event with a constant 6 words of calldata
+            // and four indexed topics: selector, caller, arg1 and arg2
+            let mark := msize()                         // end of memory ensures zero
+            mstore(0x40, add(mark, 288))              // update free memory pointer
+            mstore(mark, 0x20)                        // bytes type data offset
+            mstore(add(mark, 0x20), 224)              // bytes size (padded)
+            calldatacopy(add(mark, 0x40), 0, 224)     // bytes payload
+            log4(mark, 288,                           // calldata
+                 shl(224, shr(224, calldataload(0))), // msg.sig
+                 caller(),                              // msg.sender
+                 calldataload(4),                     // arg1
+                 calldataload(36)                     // arg2
+                )
+        }
+    }
+}
+
+interface VatLikeSpot {
+    function file(bytes32, bytes32, uint) external;
+}
+
+interface PipLikeSpot {
+    function peek() external returns (bytes32, bool);
+}
+
+contract Spotter is LibNoteSpot {
+    // --- Auth ---
+    mapping (address => uint) public wards;
+    function rely(address guy) external note auth { wards[guy] = 1;  }
+    function deny(address guy) external note auth { wards[guy] = 0; }
+    modifier auth {
+        require(wards[msg.sender] == 1, "Spotter/not-authorized");
+        _;
+    }
+
+    // --- Data ---
+    struct Ilk {
+        PipLikeSpot pip;
+        uint256 mat;
+    }
+
+    mapping (bytes32 => Ilk) public ilks;
+
+    VatLikeSpot public vat;
+    uint256 public par; // ref per dai
+
+    uint256 public live;
+
+    // --- Events ---
+    event Poke(
+      bytes32 ilk,
+      bytes32 val,
+      uint256 spot
+    );
+
+    // --- Init ---
+    constructor(address vat_) public {
+        wards[msg.sender] = 1;
+        vat = VatLikeSpot(vat_);
+        par = ONE;
+        live = 1;
+    }
+
+    // --- Math ---
+    uint constant ONE = 10 ** 27;
+
+    function mul(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+    function rdiv(uint x, uint y) internal pure returns (uint z) {
+        z = mul(x, ONE) / y;
+    }
+
+    // --- Administration ---
+    function file(bytes32 ilk, bytes32 what, address pip_) external note auth {
+        require(live == 1, "Spotter/not-live");
+        if (what == "pip") ilks[ilk].pip = PipLikeSpot(pip_);
+        else revert("Spotter/file-unrecognized-param");
+    }
+    function file(bytes32 what, uint data) external note auth {
+        require(live == 1, "Spotter/not-live");
+        if (what == "par") par = data;
+        else revert("Spotter/file-unrecognized-param");
+    }
+    function file(bytes32 ilk, bytes32 what, uint data) external note auth {
+        require(live == 1, "Spotter/not-live");
+        if (what == "mat") ilks[ilk].mat = data;
+        else revert("Spotter/file-unrecognized-param");
+    }
+
+    // --- Update value ---
+    function poke(bytes32 ilk) external {
+        (bytes32 val, bool has) = ilks[ilk].pip.peek();
+        uint256 spot = has ? rdiv(rdiv(mul(uint(val), 10 ** 9), par), ilks[ilk].mat) : 0;
+        vat.file(ilk, "spot", spot);
+        emit Poke(ilk, val, spot);
+    }
+
+    function cage() external note auth {
+        live = 0;
+    }
+}
