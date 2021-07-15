849e5d18c68bd3be460b330f8ec33505a30115fa
Skani1997 <63139286+Skani1997@users.noreply.github.com>
2021-07-15 16:53:22 +0900

    Create vow.sol

---
+++ vow.sol
@@ -0,0 +1,171 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+pragma solidity >=0.5.12;
+
+contract LibNoteVow {
+    event LogNote(
+        bytes4   indexed  sig,
+        address  indexed  usr,
+        bytes32  indexed  arg1,
+        bytes32  indexed  arg2,
+        bytes             data
+    ) anonymous;
+
+    modifier note {
+        _;
+        assembly {
+            // log an 'anonymous' event with a constant 6 words of calldata
+            // and four indexed topics: selector, caller, arg1 and arg2
+            let mark := msize()                         // end of memory ensures zero
+            mstore(0x40, add(mark, 288))              // update free memory pointer
+            mstore(mark, 0x20)                        // bytes type data offset
+            mstore(add(mark, 0x20), 224)              // bytes size (padded)
+            calldatacopy(add(mark, 0x40), 0, 224)     // bytes payload
+            log4(mark, 288,                           // calldata
+                 shl(224, shr(224, calldataload(0))), // msg.sig
+                 caller(),                              // msg.sender
+                 calldataload(4),                     // arg1
+                 calldataload(36)                     // arg2
+                )
+        }
+    }
+}
+
+abstract contract FlopLikeVow {
+    function kick(address gal, uint lot, uint bid) virtual external returns (uint);
+    function cage() virtual external;
+    function live() virtual external returns (uint);
+}
+
+abstract contract FlapLikeVow {
+    function kick(uint lot, uint bid) virtual external returns (uint);
+    function cage(uint) virtual external;
+    function live() virtual external returns (uint);
+}
+
+abstract contract VatLikeVow {
+    function dai (address) virtual external view returns (uint);
+    function sin (address ) virtual external view returns (uint);
+    function heal(uint256) virtual external;
+    function hope(address) virtual external;
+    function nope(address) virtual external;
+}
+
+contract Vow is LibNoteVow{
+    // --- Auth ---
+    mapping (address => uint) public wards;
+    function rely(address usr) external auth { require(live == 1, "Vow/not-live"); wards[usr] = 1; }
+    function deny(address usr) external auth { wards[usr] = 0; }
+    modifier auth {
+        require(wards[msg.sender] == 1, "Vow/not-authorized");
+        _;
+    }
+
+    // --- Data ---
+    VatLikeVow public vat;        // CDP Engine
+    FlapLikeVow public flapper;   // Surplus Auction House
+    FlopLikeVow public flopper;   // Debt Auction House
+
+    mapping (uint256 => uint256) public sin;  // debt queue
+    uint256 public Sin;   // Queued debt            [rad]
+    uint256 public Ash;   // On-auction debt        [rad]
+
+    uint256 public wait;  // Flop delay             [seconds]
+    uint256 public dump;  // Flop initial lot size  [wad]
+    uint256 public sump;  // Flop fixed bid size    [rad]
+
+    uint256 public bump;  // Flap fixed lot size    [rad]
+    uint256 public hump;  // Surplus buffer         [rad]
+
+    uint256 public live;  // Active Flag
+
+    // --- Init ---
+    constructor(address vat_, address flapper_, address flopper_)public{
+        wards[msg.sender] = 1;
+        vat     = VatLikeVow(vat_);
+        flapper = FlapLikeVow(flapper_);
+        flopper = FlopLikeVow(flopper_);
+        vat.hope(flapper_);
+        live = 1;
+    }
+
+    // --- Math ---
+    function add(uint x, uint y) internal pure returns (uint z) {
+        require((z = x + y) >= x);
+    }
+    function sub(uint x, uint y) internal pure returns (uint z) {
+        require((z = x - y) <= x);
+    }
+    function min(uint x, uint y) internal pure returns (uint z) {
+        return x <= y ? x : y;
+    }
+
+    // --- Administration ---
+    function file(bytes32 what, uint data) external auth {
+        if (what == "wait") wait = data;
+        else if (what == "bump") bump = data;
+        else if (what == "sump") sump = data;
+        else if (what == "dump") dump = data;
+        else if (what == "hump") hump = data;
+        else revert("Vow/file-unrecognized-param");
+    }
+
+    function file(bytes32 what, address data) external auth {
+        if (what == "flapper") {
+            vat.nope(address(flapper));
+            flapper = FlapLikeVow(data);
+            vat.hope(data);
+        }
+        else if (what == "flopper") flopper = FlopLikeVow(data);
+        else revert("Vow/file-unrecognized-param");
+    }
+
+    // Push to debt-queue
+    function fess(uint tab) external auth {
+        sin[block.timestamp] = add(sin[block.timestamp], tab);
+        Sin = add(Sin, tab);
+    }
+    // Pop from debt-queue
+    function flog(uint era) external {
+        require(add(era, wait) <= block.timestamp, "Vow/wait-not-finished");
+        Sin = sub(Sin, sin[era]);
+        sin[era] = 0;
+    }
+
+    // Debt settlement
+    function heal(uint rad) external {
+        require(rad <= vat.dai(address(this)), "Vow/insufficient-surplus");
+        require(rad <= sub(sub(vat.sin(address(this)), Sin), Ash), "Vow/insufficient-debt");
+        vat.heal(rad);
+    }
+    function kiss(uint rad) external {
+        require(rad <= Ash, "Vow/not-enough-ash");
+        require(rad <= vat.dai(address(this)), "Vow/insufficient-surplus");
+        Ash = sub(Ash, rad);
+        vat.heal(rad);
+    }
+
+    // Debt auction
+    function flop() external returns (uint id) {
+        require(sump <= sub(sub(vat.sin(address(this)), Sin), Ash), "Vow/insufficient-debt");
+        require(vat.dai(address(this)) == 0, "Vow/surplus-not-zero");
+        Ash = add(Ash, sump);
+        id = flopper.kick(address(this), dump, sump);
+    }
+    // Surplus auction
+    function flap() external returns (uint id) {
+        require(vat.dai(address(this)) >= add(add(vat.sin(address(this)), bump), hump), "Vow/insufficient-surplus");
+        require(sub(sub(vat.sin(address(this)), Sin), Ash) == 0, "Vow/debt-not-zero");
+        id = flapper.kick(bump, 0);
+    }
+
+    function cage() external auth {
+        require(live == 1, "Vow/not-live");
+        live = 0;
+        Sin = 0;
+        Ash = 0;
+        flapper.cage(vat.dai(address(flapper)));
+        flopper.cage();
+        vat.heal(min(vat.dai(address(this)), vat.sin(address(this))));
+    }
+}
