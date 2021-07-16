71a22c26921bc57c1107dfa2f34c91c93f74d0e9
Skani1997 <63139286+Skani1997@users.noreply.github.com>
2021-07-16 10:53:59 +0900

    no libnote

--- flap.sol
+++ flap.sol
@@ -5,57 +5,6 @@
 // hevm: flattened sources of /nix/store/8xb41r4qd0cjb63wcrxf1qmfg88p0961-dss-6fd7de0/src/flap.sol
 pragma solidity >=0.5.12;
 
-contract LibNoteFlap {
-    event LogNote(
-        bytes4   indexed  sig,
-        address  indexed  usr,
-        bytes32  indexed  arg1,
-        bytes32  indexed  arg2,
-        bytes             data
-    ) anonymous;
-
-    modifier note {
-        _;
-        assembly {
-            // log an 'anonymous' event with a constant 6 words of calldata
-            // and four indexed topics: selector, caller, arg1 and arg2
-            let mark := msize()                         // end of memory ensures zero
-            mstore(0x40, add(mark, 288))              // update free memory pointer
-            mstore(mark, 0x20)                        // bytes type data offset
-            mstore(add(mark, 0x20), 224)              // bytes size (padded)
-            calldatacopy(add(mark, 0x40), 0, 224)     // bytes payload
-            log4(mark, 288,                           // calldata
-                 shl(224, shr(224, calldataload(0))), // msg.sig
-                 caller(),                              // msg.sender
-                 calldataload(4),                     // arg1
-                 calldataload(36)                     // arg2
-                )
-        }
-    }
-}
-
-////// /nix/store/8xb41r4qd0cjb63wcrxf1qmfg88p0961-dss-6fd7de0/src/flap.sol
-/// flap.sol -- Surplus auction
-
-// Copyright (C) 2018 Rain <rainbreak@riseup.net>
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-/* pragma solidity 0.5.12; */
-
-/* import "./lib.sol"; */
-
 interface VatLikeFlap {
     function move(address,address,uint) external;
 }
@@ -74,11 +23,11 @@ interface GemLikeFlap {
  - `end` max auction duration
 */
 
-contract Flapper is LibNoteFlap {
+contract Flapper {
     // --- Auth ---
     mapping (address => uint) public wards;
-    function rely(address usr) external note auth { wards[usr] = 1; }
-    function deny(address usr) external note auth { wards[usr] = 0; }
+    function rely(address usr) external auth { wards[usr] = 1; }
+    function deny(address usr) external auth { wards[usr] = 0; }
     modifier auth {
         require(wards[msg.sender] == 1, "Flapper/not-authorized");
         _;
@@ -129,7 +78,7 @@ contract Flapper is LibNoteFlap {
     }
 
     // --- Admin ---
-    function file(bytes32 what, uint data) external note auth {
+    function file(bytes32 what, uint data) external auth {
         if (what == "beg") beg = data;
         else if (what == "ttl") ttl = uint48(data);
         else if (what == "tau") tau = uint48(data);
@@ -151,12 +100,12 @@ contract Flapper is LibNoteFlap {
 
         emit Kick(id, lot, bid);
     }
-    function tick(uint id) external note {
+    function tick(uint id) external {
         require(bids[id].end < block.timestamp, "Flapper/not-finished");
         require(bids[id].tic == 0, "Flapper/bid-already-placed");
         bids[id].end = add(uint48(block.timestamp), tau);
     }
-    function tend(uint id, uint lot, uint bid) external note {
+    function tend(uint id, uint lot, uint bid) external {
         require(live == 1, "Flapper/not-live");
         require(bids[id].guy != address(0), "Flapper/guy-not-set");
         require(bids[id].tic > block.timestamp || bids[id].tic == 0, "Flapper/already-finished-tic");
@@ -173,7 +122,7 @@ contract Flapper is LibNoteFlap {
         bids[id].bid = bid;
         bids[id].tic = add(uint48(block.timestamp), ttl);
     }
-    function deal(uint id) external note {
+    function deal(uint id) external {
         require(live == 1, "Flapper/not-live");
         require(bids[id].tic != 0 && (bids[id].tic < block.timestamp || bids[id].end < block.timestamp), "Flapper/not-finished");
         vat.move(address(this), bids[id].guy, bids[id].lot);
@@ -181,11 +130,11 @@ contract Flapper is LibNoteFlap {
         delete bids[id];
     }
 
-    function cage(uint rad) external note auth {
+    function cage(uint rad) external auth {
        live = 0;
        vat.move(address(this), msg.sender, rad);
     }
-    function yank(uint id) external note {
+    function yank(uint id) external {
         require(live == 0, "Flapper/still-live");
         require(bids[id].guy != address(0), "Flapper/guy-not-set");
         gem.move(address(this), bids[id].guy, bids[id].bid);
