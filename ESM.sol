8093a6a928460577a3f626f2ee592442adae44c0
Skani1997 <63139286+Skani1997@users.noreply.github.com>
2021-07-16 11:04:38 +0900

    no note plz

--- ESM.sol
+++ ESM.sol
@@ -5,9 +5,6 @@
 // hevm: flattened sources of /nix/store/pn2d4gb0yq19i7ixbxiy90933vlxhacj-esm-8e2d767/src/ESM.sol
 pragma solidity >=0.5.12;
 
-////// /nix/store/pn2d4gb0yq19i7ixbxiy90933vlxhacj-esm-8e2d767/src/ESM.sol
-/* pragma solidity ^0.5.6; */
-
 interface GemLikeESM {
     function balanceOf(address) external view returns (uint256);
     function transfer(address, uint256) external returns (bool);
@@ -28,34 +25,6 @@ contract ESM {
     mapping(address => uint256) public sum; // per-address balance
     uint256 public Sum; // total balance
 
-    // --- Logs ---
-    event LogNote(
-        bytes4   indexed  sig,
-        address  indexed  usr,
-        bytes32  indexed  arg1,
-        bytes32  indexed  arg2,
-        bytes             data
-    ) anonymous;
-
-    modifier note {
-        _;
-        assembly {
-            // log an 'anonymous' event with a constant 6 words of calldata
-            // and four indexed topics: selector, caller, arg1 and arg2
-            let mark := msize()                         // end of memory ensures zero
-            mstore(0x40, add(mark, 288))              // update free memory pointer
-            mstore(mark, 0x20)                        // bytes type data offset
-            mstore(add(mark, 0x20), 224)              // bytes size (padded)
-            calldatacopy(add(mark, 0x40), 0, 224)     // bytes payload
-            log4(mark, 288,                           // calldata
-                 shl(224, shr(224, calldataload(0))), // msg.sig
-                 caller(),                              // msg.sender
-                 calldataload(4),                     // arg1
-                 calldataload(36)                     // arg2
-                )
-        }
-    }
-
     constructor(address gem_, address end_, address pit_, uint256 min_) public {
         gem = GemLikeESM(gem_);
         end = EndLikeESM(end_);
@@ -69,7 +38,7 @@ contract ESM {
         require(z >= x);
     }
 
-    function fire() external note {
+    function fire() external {
         require(fired == 0,  "esm/already-fired");
         require(Sum >= min,  "esm/min-not-reached");
 
@@ -78,7 +47,7 @@ contract ESM {
         fired = 1;
     }
 
-    function join(uint256 wad) external note {
+    function join(uint256 wad) external {
         require(fired == 0, "esm/already-fired");
 
         sum[msg.sender] = add(sum[msg.sender], wad);
