7755f398da600d4a586a20d6fdef413a35d49a5a
Skani1997 <63139286+Skani1997@users.noreply.github.com>
2021-07-21 17:44:06 +0900

    Update flop.sol

--- flop.sol
+++ flop.sol
@@ -120,8 +120,8 @@ contract Flopper{
 
             // on first dent, clear as much Ash as possible
             if (bids[id].tic == 0) {
-                uint Ash = VowLike(bids[id].guy).Ash();
-                VowLike(bids[id].guy).kiss(min(bid, Ash));
+                uint Ash = VowLikeFlop(bids[id].guy).Ash();
+                VowLikeFlop(bids[id].guy).kiss(min(bid, Ash));
             }
 
             bids[id].guy = msg.sender;
