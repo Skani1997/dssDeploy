3f0d9886a03f9661411a86c6a8b0483f3e578987
Skani1997 <63139286+Skani1997@users.noreply.github.com>
2021-07-29 18:16:58 +0900

    Update vat.sol

--- vat.sol
+++ vat.sol
@@ -3,7 +3,7 @@
 */
 
 // hevm: flattened sources of /nix/store/8xb41r4qd0cjb63wcrxf1qmfg88p0961-dss-6fd7de0/src/vat.sol
-pragma solidity =0.5.12;
+pragma solidity >=0.5.12;
 
 ////// /nix/store/8xb41r4qd0cjb63wcrxf1qmfg88p0961-dss-6fd7de0/src/vat.sol
 /// vat.sol -- Dai CDP database
