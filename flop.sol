57dfdba06fbf5296631600e16ad7404f41a31015
Skani1997 <63139286+Skani1997@users.noreply.github.com>
2021-07-29 16:42:51 +0900

    Update flop.sol

--- flop.sol
+++ flop.sol
@@ -1,28 +1,83 @@
 /**
- *Submitted for verification at Etherscan.io on 2019-11-14
+ *Submitted for verification at Etherscan.io on 2020-07-22
 */
 
-// hevm: flattened sources of /nix/store/8xb41r4qd0cjb63wcrxf1qmfg88p0961-dss-6fd7de0/src/flop.sol
+/// flop.sol -- Debt auction
+
+// Copyright (C) 2018 Rain <rainbreak@riseup.net>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 pragma solidity >=0.5.12;
 
-interface VatLikeFlop {
+contract LibNote {
+    event LogNote(
+        bytes4   indexed  sig,
+        address  indexed  usr,
+        bytes32  indexed  arg1,
+        bytes32  indexed  arg2,
+        bytes             data
+    ) anonymous;
+
+    modifier note {
+        _;
+        assembly {
+            // log an 'anonymous' event with a constant 6 words of calldata
+            // and four indexed topics: selector, caller, arg1 and arg2
+            let mark := msize()                       // end of memory ensures zero
+            mstore(0x40, add(mark, 288))              // update free memory pointer
+            mstore(mark, 0x20)                        // bytes type data offset
+            mstore(add(mark, 0x20), 224)              // bytes size (padded)
+            calldatacopy(add(mark, 0x40), 0, 224)     // bytes payload
+            log4(mark, 288,                           // calldata
+                 shl(224, shr(224, calldataload(0))), // msg.sig
+                 caller(),                            // msg.sender
+                 calldataload(4),                     // arg1
+                 calldataload(36)                     // arg2
+                )
+        }
+    }
+}
+
+interface VatLike {
     function move(address,address,uint) external;
     function suck(address,address,uint) external;
 }
-interface GemLikeFlop {
+interface GemLike {
     function mint(address,uint) external;
 }
-
-interface VowLikeFlop {
+interface VowLike {
     function Ash() external returns (uint);
     function kiss(uint) external;
 }
 
-contract Flopper{
+/*
+   This thing creates gems on demand in return for dai.
+
+ - `lot` gems in return for bid
+ - `bid` dai paid
+ - `gal` receives dai income
+ - `ttl` single bid lifetime
+ - `beg` minimum bid increase
+ - `end` max auction duration
+*/
+
+contract Flopper is LibNote {
     // --- Auth ---
     mapping (address => uint) public wards;
-    function rely(address usr) external auth { wards[usr] = 1; }
-    function deny(address usr) external auth { wards[usr] = 0; }
+    function rely(address usr) external note auth { wards[usr] = 1; }
+    function deny(address usr) external note auth { wards[usr] = 0; }
     modifier auth {
         require(wards[msg.sender] == 1, "Flopper/not-authorized");
         _;
@@ -30,27 +85,27 @@ contract Flopper{
 
     // --- Data ---
     struct Bid {
-        uint256 bid;
-        uint256 lot;
+        uint256 bid;  // dai paid                [rad]
+        uint256 lot;  // gems in return for bid  [wad]
         address guy;  // high bidder
-        uint48  tic;  // expiry time
-        uint48  end;
+        uint48  tic;  // bid expiry time         [unix epoch time]
+        uint48  end;  // auction expiry time     [unix epoch time]
     }
 
     mapping (uint => Bid) public bids;
 
-    VatLikeFlop  public   vat;
-    GemLikeFlop  public   gem;
+    VatLike  public   vat;  // CDP Engine
+    GemLike  public   gem;
 
     uint256  constant ONE = 1.00E18;
     uint256  public   beg = 1.05E18;  // 5% minimum bid increase
     uint256  public   pad = 1.50E18;  // 50% lot increase for tick
-    uint48   public   ttl = 3 hours;  // 3 hours bid lifetime
-    uint48   public   tau = 2 days;   // 2 days total auction length
+    uint48   public   ttl = 3 hours;  // 3 hours bid lifetime         [seconds]
+    uint48   public   tau = 2 days;   // 2 days total auction length  [seconds]
     uint256  public kicks = 0;
-    uint256  public live;
+    uint256  public live;             // Active Flag
     address  public vow;              // not used until shutdown
-    
+
     // --- Events ---
     event Kick(
       uint256 id,
@@ -62,8 +117,8 @@ contract Flopper{
     // --- Init ---
     constructor(address vat_, address gem_) public {
         wards[msg.sender] = 1;
-        vat = VatLikeFlop(vat_);
-        gem = GemLikeFlop(gem_);
+        vat = VatLike(vat_);
+        gem = GemLike(gem_);
         live = 1;
     }
 
@@ -79,7 +134,7 @@ contract Flopper{
     }
 
     // --- Admin ---
-    function file(bytes32 what, uint data) external auth {
+    function file(bytes32 what, uint data) external note auth {
         if (what == "beg") beg = data;
         else if (what == "pad") pad = data;
         else if (what == "ttl") ttl = uint48(data);
@@ -100,13 +155,13 @@ contract Flopper{
 
         emit Kick(id, lot, bid, gal);
     }
-    function tick(uint id) external {
+    function tick(uint id) external note {
         require(bids[id].end < block.timestamp, "Flopper/not-finished");
         require(bids[id].tic == 0, "Flopper/bid-already-placed");
         bids[id].lot = mul(pad, bids[id].lot) / ONE;
         bids[id].end = add(uint48(block.timestamp), tau);
     }
-    function dent(uint id, uint lot, uint bid) external {
+    function dent(uint id, uint lot, uint bid) external note {
         require(live == 1, "Flopper/not-live");
         require(bids[id].guy != address(0), "Flopper/guy-not-set");
         require(bids[id].tic > block.timestamp || bids[id].tic == 0, "Flopper/already-finished-tic");
@@ -121,29 +176,29 @@ contract Flopper{
 
             // on first dent, clear as much Ash as possible
             if (bids[id].tic == 0) {
-                uint Ash = VowLikeFlop(bids[id].guy).Ash();
-                VowLikeFlop(bids[id].guy).kiss(min(bid, Ash));
+                uint Ash = VowLike(bids[id].guy).Ash();
+                VowLike(bids[id].guy).kiss(min(bid, Ash));
             }
 
             bids[id].guy = msg.sender;
         }
 
         bids[id].lot = lot;
-        bids[id].tic = add(uint48(now), ttl);
+        bids[id].tic = add(uint48(block.timestamp), ttl);
     }
-    function deal(uint id) external {
+    function deal(uint id) external note {
         require(live == 1, "Flopper/not-live");
         require(bids[id].tic != 0 && (bids[id].tic < block.timestamp || bids[id].end < block.timestamp), "Flopper/not-finished");
         gem.mint(bids[id].guy, bids[id].lot);
         delete bids[id];
     }
 
-    // shutdown
-    function cage() external auth {
+    // --- Shutdown ---
+    function cage() external note auth {
        live = 0;
        vow = msg.sender;
     }
-    function yank(uint id) external {
+    function yank(uint id) external note {
         require(live == 0, "Flopper/still-live");
         require(bids[id].guy != address(0), "Flopper/guy-not-set");
         vat.suck(vow, bids[id].guy, bids[id].bid);
